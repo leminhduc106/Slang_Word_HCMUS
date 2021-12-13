2537145c7454ba6bb3286e203ab7d8977cfc55c3
minhduc <leminhduc1062001@gmail.com>
2021-12-13 14:00:35 +0700

    add images folder

--- Dict_19127369/src/dict_19127369/FileProccess.java
+++ Dict_19127369/src/dict_19127369/FileProccess.java
@@ -38,6 +38,27 @@ public class FileProccess {
         }
     }
     
+    public static Object[][] readHistory(String path){
+        ArrayList<Object[]> data = new ArrayList<>();
+        File file = new File(path);
+        try {
+             Scanner scan = new Scanner(file);
+             while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                try {
+                    String[] lineSplit = line.split("`");
+                    Object[] lineData = {lineSplit[0], lineSplit[1]};
+                    data.add(lineData);
+                } catch (Exception e) {
+                }
+            }
+           
+        } catch (Exception e) {
+        }
+        Object[][] result = new Object[data.size()][];
+        return data.toArray(result);
+    }
+    
     public static void write(String path, SlangWordList list){
         BufferedWriter bw = null;
         FileWriter fw = null;
@@ -68,6 +89,22 @@ public class FileProccess {
         } catch (IOException e) {
                 e.printStackTrace();
                 
+        }
+    }
+    
+    public static void writeHistory(String path, String str1, String str2){
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        try {
+                fw = new FileWriter(path, true);
+                bw = new BufferedWriter(fw);
+                String line = str1 + "`" + str2;
+                bw.write(line);
+                bw.newLine();
+                bw.close();
+        } catch (IOException e) {
+                e.printStackTrace();
+          
         }
     }
 }
